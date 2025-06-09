3a82cc47a928b6f2db7205c1a6c8ee56d8a7f4eb
O-darel <oballadarel@gmail.com>
2025-06-09 16:04:02 +0300

    Initial spring boot setup

---
+++ dog-palace/dog-palace/src/main/java/dev/darel/dog_palace/DogPalaceApplication.java
@@ -0,0 +1,13 @@
+package dev.darel.dog_palace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogPalaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogPalaceApplication.class, args);
+	}
+
+}
---
+++ dog-palace/dog-palace/src/test/java/dev/darel/dog_palace/DogPalaceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.darel.dog_palace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogPalaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
